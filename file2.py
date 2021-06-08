09cbadf0b80a90261111d945c2715d4e4ef73a49
deekshitha0211 <85266022+deekshitha0211@users.noreply.github.com>
2021-06-08 12:57:24 +0530

    Update file2.py

--- file2.py
+++ file2.py
@@ -1,7 +1,5 @@
 import os
 	list1 = [8, 9, 3, 6, 1, 0]
 	list1.reverse()
-	print("The Reversed list is",list1)import os
-	list1 = [8, 9, 3, 6, 1, 0]
-	list1.reverse()
+	print("The Reversed list is",list1)
 
